e71fa9595fa33567cbeac397585b53da07b843ae
An Tran <anbtran98@gmail.com>
2025-01-05 11:31:08 -0600

    Getting Started - Shader: Rainbow triangle by setting color at each point of the vertices

--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,19 @@ const unsigned int SCREEN_HEIGHT = 600;
     // Create vertex shader source and object
     const char *vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
-        // "out vec4 vertextColor;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
         "void main(){\n"
-        "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        // "    vertextColor = vec4(1.0, 0.5, 0.2, 0.1);\n"
+        "    gl_Position = vec4(aPos, 1.0);\n"
+        "    ourColor = aColor;\n"
         "}\0";
 
 /* Fragment Shader  */
 const char *fragmentShaderSource = "#version 330 core\n"
-    // "in vec4 vertextColor;\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main(){\n"
-    // "    FragColor = vertextColor;\n"
-    "    FragColor = ourColor;\n"
+    "    FragColor = vec4(ourColor, 1.0);\n"
     "}\0";
 
 int main() {
@@ -53,9 +52,10 @@ int main() {
 
     // VBO example without EBO
     float vertices[] = {
-        0.0f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+         // Positions        // Colors
+         0.0f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // top
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom left
+         0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f // bottom right
     };
 
     /* VBO */
@@ -72,8 +72,10 @@ int main() {
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
     /* Linking Vertex Attributes Section */
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0 );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0 );
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)) );
+    glEnableVertexAttribArray(1);
 
     unsigned int vertexShader;
     vertexShader = glCreateShader( GL_VERTEX_SHADER );
@@ -127,10 +129,6 @@ int main() {
         glClear( GL_COLOR_BUFFER_BIT );
 
         glUseProgram( shaderProgram );
-        float timeVal = glfwGetTime();
-        float greenValue = sin(timeVal) / 2.0f + 0.5f;
-        int vertextColorLocation = glGetUniformLocation( shaderProgram, "ourColor" );
-        glUniform4f( vertextColorLocation, 0.0f, greenValue, 0.0f, 1.0f );
         glBindVertexArray(VAO);
         glDrawArrays( GL_TRIANGLES, 0, 3 );
 
