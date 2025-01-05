9ebaf7410371aec63549a0f7c956ab120d7d30b7
An Tran <anbtran98@gmail.com>
2025-01-05 16:46:12 -0600

    Added Shader Class; control color by changing individual rgb color or with vec4 (Uniform variables)

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,13 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include "include/shader.h"
 
 void processInput( GLFWwindow* window );
 void framebuffer_size_callback( GLFWwindow* window, int width, int height );
 
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
-
-    /* Vertex Shader  */
-    // Create vertex shader source and object
-    const char *vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "out vec3 ourColor;\n"
-        "void main(){\n"
-        "    gl_Position = vec4(aPos, 1.0);\n"
-        "    ourColor = aColor;\n"
-        "}\0";
-
-/* Fragment Shader  */
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main(){\n"
-    "    FragColor = vec4(ourColor, 1.0);\n"
-    "}\0";
 
 int main() {
 
@@ -52,10 +34,10 @@ int main() {
 
     // VBO example without EBO
     float vertices[] = {
-         // Positions        // Colors
-         0.0f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // top
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom left
-         0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f // bottom right
+         // Positions
+         0.0f, 0.5f, 0.0f,  // top
+        -0.5f, -0.5f, 0.0f, // bottom left
+         0.5f, -0.5f, 0.0f  // bottom right
     };
 
     /* VBO */
@@ -71,53 +53,10 @@ int main() {
     // Bind the object to a buffer type
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
-    /* Linking Vertex Attributes Section */
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0 );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0 );
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)) );
-    glEnableVertexAttribArray(1);
 
-    unsigned int vertexShader;
-    vertexShader = glCreateShader( GL_VERTEX_SHADER );
-    // Load the source and compile
-    glShaderSource( vertexShader, 1, &vertexShaderSource, NULL );
-    glCompileShader( vertexShader );
-    // Error Logging
-    int success;
-    char infoLog[512];
-    glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &success );
-    if (!success) {
-        glGetShaderInfoLog( vertexShader, 512, NULL, infoLog );
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-    glShaderSource( fragmentShader, 1, &fragmentShaderSource, NULL );
-    glCompileShader( fragmentShader );
-    //Error logging
-    glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &success );
-    if (!success) {
-        glGetShaderInfoLog( fragmentShader, 512, NULL, infoLog );
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION::FAILED\n" << infoLog << std::endl;
-    }
-
-    /* Shader Program */
-    // Create shader program object
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    // attach and link the 2 shader above into shader program
-    glAttachShader( shaderProgram, vertexShader );
-    glAttachShader( shaderProgram, fragmentShader );
-    glLinkProgram( shaderProgram );
-    // Error Logging
-    glGetProgramiv( shaderProgram, GL_LINK_STATUS, &success );
-    if (!success) {
-        glGetProgramInfoLog( shaderProgram, 512, NULL, infoLog );
-        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION::FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader( vertexShader );
-    glDeleteShader( fragmentShader );
+    Shader ourShader("src/vsShader.vs", "src/fShader.fs");
 
     while ( !glfwWindowShouldClose(window) ) {
         /* Update  */
@@ -128,7 +67,13 @@ int main() {
         glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
 
-        glUseProgram( shaderProgram );
+        // glUseProgram( shaderProgram );
+        ourShader.use();
+        // ourShader.setColorUni4f( "myColor", 0.7f, 0.1f, 0.7f ); // for Uniform vec4 variable
+        ourShader.setFloat( "r", 1.0f );
+        ourShader.setFloat( "g", 0.0f );
+        ourShader.setFloat( "b", 1.0f );
+        ourShader.setFloat( "a", 1.0f );
         glBindVertexArray(VAO);
         glDrawArrays( GL_TRIANGLES, 0, 3 );
 
@@ -139,7 +84,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
+    glDeleteProgram(ourShader.ID);
 
     glfwTerminate();
     return 0;
