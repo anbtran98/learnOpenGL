a0525a5c7e94ee2eef67729890627ebe8ffa2b7b
An Tran <anbtran98@gmail.com>
2025-06-01 21:07:34 -0500

    Getting-Started-Coordinate System: 10 cubes & 3 exercises

--- src/main.cpp
+++ src/main.cpp
@@ -141,6 +141,19 @@ int main() {
         1, 2, 3
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     /* VBO */
     unsigned int VBO;
     glGenBuffers( 1, &VBO );
@@ -172,7 +185,7 @@ int main() {
     ourShader.use();
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
-    ourShader.setFloat("transInterpolation", 0.2f);
+    ourShader.setFloat("transInterpolation", 0.3f);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -194,15 +207,20 @@ int main() {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 1.0f, 1.0f));
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::translate(view, glm::vec3(2.0f, -1.0f, -8.0f));
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(30.0f), 800.0f / 480.0f, 0.1f, 100.0f);
 
-        int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        /*
+            Coordinate System Section Exercise 1,2
+            Observation:
+            1) View translation: Certain cube is clipped from the final view
+            2) Changing the projection:
+                - Aspect-ratio: 16:9 squish the cubes, 4:3 regular cubes
+                - FOV: large number make cubs smaller, and vice versa
+        */
+
         int viewLoc = glGetUniformLocation(ourShader.ID, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
@@ -210,8 +228,17 @@ int main() {
 
         ourShader.use();
         glBindVertexArray(VAO);
-        /*glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );*/
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            if (i % 3 == 0) { // Coordinate System Section Exercise 3
+                model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+            }
+
+            int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         /* Check and call events and swap the buffers */
         glfwSwapBuffers( window );
