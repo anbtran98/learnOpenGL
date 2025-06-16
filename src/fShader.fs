02cc069d19f6c09c0543bcffca57e201b98203c1
An Tran <anbtran98@gmail.com>
2025-06-16 16:50:39 -0500

    Lighting - Light Casters: Spotlights

--- src/fShader.fs
+++ src/fShader.fs
@@ -8,10 +8,13 @@ struct Material {
 
 struct Light {
        vec3 position;
-       //vec3 direction;
+       vec3 direction;
+       float cutOff;
+
        vec3 ambient;
        vec3 diffuse;
        vec3 specular;
+
        float constant;
        float linear;
        float quadratic;
@@ -33,29 +36,34 @@ uniform vec3 viewPos;
 */
 
 void main() {
-    float distance = length(light.position - FragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-
-    // ambient
-    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
-  	
-    // diffuse 
-    vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
-    //vec3 lightDir = normalize(-light.direction);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
-    
-    // specular
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
-     
-    ambient *= attenuation;
-    diffuse *= attenuation;
-    specular *= attenuation;
+    float theta = dot(lightDir, normalize(-light.direction));
+    if (theta > light.cutOff) {
+        float distance = length(light.position - FragPos);
+        float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
 
-    vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+        // ambient
+        vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+  	
+        // diffuse 
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+    
+        // specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);  
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+     
+        ambient *= attenuation;
+        diffuse *= attenuation;
+        specular *= attenuation;
+
+        vec3 result = ambient + diffuse + specular;
+        FragColor = vec4(result, 1.0);
+    } else
+        FragColor = vec4(light.ambient * vec3(texture(material.diffuse, TexCoords)), 1.0);
+
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -193,14 +193,18 @@ int main() {
         glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
         // Set fixed light color
-        ourShader.setVec3("light.position", lightPos);
-        //ourShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
-        ourShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
-        ourShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // darken diffuse light a bit
+        ourShader.setVec3("light.position", cameraPos);
+        ourShader.setVec3("light.direction", cameraFront);
+        ourShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+
+        ourShader.setVec3("light.ambient",  0.1f, 0.1f, 0.1f);
+        ourShader.setVec3("light.diffuse",  0.8f, 0.8f, 0.8f); // darken diffuse light a bit
         ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
         ourShader.setFloat("light.constant", 1.0f);
         ourShader.setFloat("light.linear", 0.09f);
         ourShader.setFloat("light.quadratic", 0.032f);
+
         ourShader.setFloat("material.shininess", 64.0f);
 
         ourShader.setVec3("viewPos", cameraPos);
